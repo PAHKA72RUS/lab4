318bbc076cd6ec9c32b25e7aaa2395eb10b3e043
PAHKA72RUS <65212376+PAHKA72RUS@users.noreply.github.com>
2020-05-29 17:27:31 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList obyect = new ArrayList() { 32, 25.3, 3, "l", 9.7, "t", "Z", "Q", 855, 1, "j", "H", 15, 14.5, 0 };
+            ArrayList obyectVverh = new ArrayList(100);
+
+            for (int i = 0; i < obyect.Count; i++)
+            {
+                Console.Write(obyect[i] + " | ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < obyect.Count; i++)
+            {
+                if (obyect[i] is string)
+                {
+                    obyect.RemoveAt(i);
+                    if (obyect[i] is string) i--;
+                }
+            }
+
+            for (int i = 0; i < obyect.Count; i++)
+            {
+                Console.Write(obyect[i] + " | ");
+            }
+        }
+    }
+}
